53d55dbe4f0460fc4e26c32e4ce710148cfdd80b
dsillman2000 <dsillman2000@gmail.com>
2023-10-21 10:15:39 -0700

    Unit to property tests (#2)
    * converting some unit tests to property tests
    
    * refactoring remaining tests
    
    * wrapping BTree in a package
    
    * adding more docs, improvements

---
+++ src/main/scala/btree/BTree.scala
@@ -0,0 +1,228 @@
+import scala.collection._
+import scala.annotation.tailrec
+
+package btree {
+
+  /** B-Tree implementation, generic in its key type `K`.
+    *
+    * @param _keys
+    *   Sequence of keys in the root node of the tree.
+    * @param _children
+    *   Sequence of subtrees between each key in the root node of the tree.
+    * @param ord
+    *   Ordering of the keys which organizes the tree.
+    * @param m
+    *   Maximum branching factor of the tree. Defaults to 5.
+    */
+  class BTree[K](var _keys: Seq[K], var _children: Seq[BTree[K]])(
+      implicit val ord: Ordering[K],
+      implicit val m: Int = 5
+  ) {
+
+    import ord._;
+
+    requireBTreeInvariants
+
+    def keys: Seq[K] = _keys
+    def children: Seq[BTree[K]] = _children
+    def isLeaf: Boolean = children.isEmpty
+    def isEmpty: Boolean = keys.isEmpty
+    def isFull: Boolean = keys.length == m - 1
+
+    /** Traverse the B-Tree in-order.
+      *
+      * @return
+      *   Elements of this B-Tree, in-order.
+      */
+    def elementsInOrder: Seq[K] = {
+      if (isLeaf) {
+        return keys
+      }
+      return children.map(_.elementsInOrder).zip(keys).flatMap(x => x._1 :+ x._2) ++ children.last.elementsInOrder
+    }
+
+    /** Asserts that the B-Tree invariants hold on this B-Tree instance.
+      */
+    private def requireBTreeInvariants = {
+      try {
+        require(m > 0, "B-Tree order, `m`, must be positive")
+        require(children.length <= m, "B-Tree must have `|C| <= m`")
+        require(
+          (keys.length == children.length - 1) || isLeaf,
+          "B-Tree node must have exactly `|C| - 1` keys"
+        )
+        require(
+          (keys.sorted == keys),
+          "B-Tree keys must be sorted"
+        )
+        require(children.filter(_.isEmpty).isEmpty, "B-Tree child nodes must have at least 1 key")
+        require(
+          children.map(_.keys(0)).sorted == children.map(_.keys(0)),
+          "B-Tree child nodes must be sorted by head key"
+        )
+        require(elementsInOrder == elementsInOrder.sorted, "B-Tree elements must be sorted, when traversed in-order")
+      } catch {
+        case e: IllegalArgumentException => throw new IllegalArgumentException(e.getMessage ++ " in:\n" ++ toString)
+      }
+    }
+
+    /** Represent this tree as a `String`.
+      *
+      * @return
+      *   `String` representation of the tree
+      */
+    override def toString: String =
+      "(" ++ (children
+        .grouped(1)
+        .zipAll(keys.grouped(1), Nil, Nil)
+        .flatMap(x => x._1 ++ x._2)
+        .mkString(",")) ++ ")"
+
+    /** Inserts a given key from the root of the tree, in-place.
+      *
+      * @param k
+      *   Key to insert into the tree
+      */
+    def insert(k: K): Unit = {
+
+      if (isFull) {
+
+        if (isLeaf) {
+          splitRoot
+        }
+
+        val insertionIdx: Int = keys.takeWhile(_ <= k).length
+        children(insertionIdx).insert(k)
+
+      } else {
+        insertNotFull(k)
+
+      }
+      requireBTreeInvariants
+    }
+
+    /** Inserts a given key from the root of the tree (assuming it is not full), in-place.
+      *
+      * @param k
+      *   Key to insert into the tree
+      */
+    private def insertNotFull(k: K): Unit = {
+      var leftKeys: Seq[K] = keys.takeWhile(_ <= k)
+      if (isLeaf) {
+        val rightKeys: Seq[K] = keys.dropWhile(_ <= k)
+        val newKeys: Seq[K] = (leftKeys :+ k) ++ rightKeys
+        _keys = newKeys
+      } else {
+        if (children(leftKeys.length).keys.length == m - 1) {
+          splitChild(leftKeys.length)
+          if (keys(leftKeys.length) < k) {
+            leftKeys = leftKeys ++ Seq(k)
+          }
+        }
+        children(leftKeys.length).insert(k)
+      }
+    }
+
+    /** Utility method for splitting a full root node, in-place.
+      */
+    private def splitRoot: Unit = {
+
+      require(isFull)
+
+      var newRoot: BTree[K] = BTree.empty(m)
+      newRoot._children = Seq(new BTree(keys, children))
+
+      newRoot.splitChild(0)
+
+      _keys = newRoot.keys
+      _children = newRoot.children
+
+      requireBTreeInvariants
+
+    }
+
+    /** Utility method for splitting an over-full child node of the root by index, in-place.
+      *
+      * @param childIdx
+      *   the index of the child node to split
+      */
+    private def splitChild(childIdx: Int) = {
+
+      if (isFull) {
+
+        splitRoot
+
+      } else {
+
+        var child: BTree[K] = children(childIdx)
+
+        require(child.isFull)
+
+        var newChild: BTree[K] = BTree.empty(m)
+        val newKey: K = child.keys(m / 2)
+        val newKeys: Seq[K] = keys.slice(0, childIdx) ++ Seq(newKey) ++ keys.slice(childIdx, m - 1)
+        val newLKeys: Seq[K] = child.keys.slice(0, m / 2)
+        val newRKeys: Seq[K] = child.keys.slice(m / 2 + 1, m)
+
+        child._keys = newLKeys
+        newChild._keys = newRKeys
+
+        if (!child.isLeaf) {
+
+          val newLChildren: Seq[BTree[K]] = child.children.slice(0, (m / 2.0).ceil.toInt)
+          val newRChildren: Seq[BTree[K]] = child.children.slice((m / 2.0).ceil.toInt, m)
+
+          child._children = newLChildren
+          newChild._children = newRChildren
+
+        }
+
+        val newChildren: Seq[BTree[K]] = (
+          children.slice(0, childIdx) ++
+            Seq(child, newChild) ++
+            children.slice(childIdx + 1, m)
+        )
+
+        _keys = newKeys
+        _children = newChildren
+
+      }
+
+      requireBTreeInvariants
+    }
+
+    /** Query the tree to search for the given key.
+      *
+      * @param k
+      *   search key
+      * @return
+      *   true if the key is found, false otherwise
+      */
+    def contains(k: K): Boolean = {
+      if (keys.isEmpty) return false
+      if (children.isEmpty) return keys.contains(k)
+      if (k < keys(0)) {
+        return children(0).contains(k)
+      }
+      for (idx <- 0 until keys.length) {
+        if (k == keys(idx)) return true
+        if (
+          idx < keys.length - 1 &&
+          k > keys(idx) &&
+          k < keys(idx + 1)
+        ) return children(idx + 1).contains(k)
+      }
+      if (k > keys.last) {
+        return children.last.contains(k)
+      }
+      return false
+    }
+
+  }
+
+  /** Companion object, useful for instantiating empty B-Trees.
+    */
+  object BTree {
+    def empty[K](m: Int = 5)(implicit ord: Ordering[K]): BTree[K] = new BTree[K](Nil, Nil)(ord, m)
+  }
+}
---
+++ src/test/scala/btree/BTreeTest.scala
@@ -0,0 +1,218 @@
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalacheck._
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+import org.scalatest.PrivateMethodTester
+
+import btree.BTree
+
+class BTreeTest extends AnyFlatSpec with ScalaCheckPropertyChecks with PrivateMethodTester {
+
+  behavior of "BTree"
+
+  it should "be instantiable as empty" in {
+    val emp: BTree[Int] = BTree.empty(m = 5)
+    assert(emp.keys.isEmpty)
+    assert(emp.children.isEmpty)
+
+    forAll((i: Int) => !emp.contains(i))
+  }
+
+  it should "be able to split a full leaf" in {
+
+    val splitChild = PrivateMethod[Unit]('splitChild)
+
+    /** Split leaf on the left of root
+      */
+    forAll { (i: Int) =>
+      whenever(i > Int.MinValue + 1 && i < Int.MaxValue - 12) {
+
+        val elems: Seq[Int] = Seq(0, 0, 1, 2, 6, 9, 11).map(_ + i)
+        var rt: BTree[Int] = new BTree(
+          Seq(elems(4)),
+          Seq(
+            new BTree(elems.slice(0, 4), Nil),
+            new BTree(elems.slice(5, 7), Nil)
+          )
+        )
+
+        rt invokePrivate splitChild(0)
+
+        assert(rt.children.length == 3)
+        assert(rt.toString == "((%s,%s),%s,(%s),%s,(%s,%s))".format(elems: _*) && elems == rt.elementsInOrder)
+
+      }
+    }
+
+    /** Split leaf on the right of root
+      */
+    forAll { (i: Int) =>
+      whenever(i > Int.MinValue + 2 && i < Int.MaxValue - 9) {
+
+        val elems: Seq[Int] = Seq(-1, -1, 0, 3, 4, 7, 8).map(_ + i)
+        var rt: BTree[Int] = new BTree(
+          Seq(elems(2)),
+          Seq(
+            new BTree(elems.slice(0, 2), Nil),
+            new BTree(elems.slice(3, 7), Nil)
+          )
+        )
+
+        rt invokePrivate splitChild(1)
+
+        assert(rt.children.length == 3)
+        assert(
+          rt.toString == "((%s,%s),%s,(%s,%s),%s,(%s))".format(elems: _*) &&
+            elems == rt.elementsInOrder
+        )
+
+      }
+    }
+
+  }
+
+  it should "be able to split a full node with singleton root" in {
+
+    val splitChild = PrivateMethod[Unit]('splitChild)
+
+    forAll { (i: Int) =>
+      whenever(i > Int.MinValue + 7 && i < Int.MaxValue - 33) {
+
+        val elems: Seq[Int] = Seq(-6, -6, -5, 0, 2, 3, 7, 10, 10, 12, 15, 18, 19, 20, 31, 32).map(_ + i)
+
+        var rt: BTree[Int] = new BTree(
+          Seq(elems(13)),
+          Seq(
+            new BTree(
+              Seq(elems(2), elems(5), elems(8), elems(11)),
+              Seq(
+                new BTree(elems.slice(0, 2), Nil),
+                new BTree(elems.slice(3, 5), Nil),
+                new BTree(elems.slice(6, 8), Nil),
+                new BTree(elems.slice(9, 11), Nil),
+                new BTree(Seq(elems(12)), Nil)
+              )
+            ),
+            new BTree(elems.slice(14, 16), Nil)
+          )
+        )
+
+        rt invokePrivate splitChild(0)
+
+        assert(
+          rt.toString == "(((%s,%s),%s,(%s,%s),%s,(%s,%s)),%s,((%s,%s),%s,(%s)),%s,(%s,%s))"
+            .format(elems: _*) &&
+            elems == rt.elementsInOrder
+        )
+
+      }
+    }
+  }
+
+  it should "be able to split a full node with full root parent" in {
+
+    val splitChild = PrivateMethod[Unit]('splitChild)
+
+    forAll { (i: Int) =>
+      whenever(i > Int.MinValue + 10 && i < Int.MaxValue - 72) {
+
+        val elems: Seq[Int] = Seq(
+          -9, -8, -6, -6, -5, -2, 0, 2, 2, 3, 4, 5, 7, 10, 10, 12, 15, 15, 16, 18, 18, 19, 19, 20, 20, 31, 32, 35, 36,
+          40, 41, 45, 50, 55, 59, 60, 65, 70, 71
+        ).map(_ + i)
+
+        var rt: BTree[Int] = new BTree(
+          Seq(elems(24), elems(29), elems(32), elems(35)),
+          Seq(
+            new BTree(
+              Seq(elems(4), elems(9), elems(14), elems(19)),
+              Seq(
+                new BTree(elems.slice(0, 4), Nil),
+                new BTree(elems.slice(5, 9), Nil),
+                new BTree(elems.slice(10, 14), Nil),
+                new BTree(elems.slice(15, 19), Nil),
+                new BTree(elems.slice(20, 24), Nil)
+              )
+            ),
+            new BTree(elems.slice(25, 29), Nil),
+            new BTree(elems.slice(30, 32), Nil),
+            new BTree(elems.slice(33, 35), Nil),
+            new BTree(elems.slice(36, 39), Nil)
+          )
+        )
+
+        rt invokePrivate splitChild(0)
+
+        assert(
+          rt.toString == ("((((%s,%s,%s,%s),%s,(%s,%s,%s,%s),%s,(%s,%s,%s,%s),%s,(%s,%s,%s,%s),%s,(%s,%s," +
+            "%s,%s)),%s,(%s,%s,%s,%s),%s,(%s,%s)),%s,((%s,%s),%s,(%s,%s,%s)))")
+            .format(elems: _*) &&
+            elems == rt.elementsInOrder
+        )
+
+      }
+    }
+  }
+
+  it should "be able to insert into a non-full leaf" in {
+
+    val insertNotFull = PrivateMethod[Unit]('insertNotFull)
+
+    forAll { (i: Int) =>
+      whenever(i < Int.MaxValue - 8) {
+
+        var rt: BTree[Int] = new BTree(Seq(i, i + 4, i + 8), Nil)
+
+        rt invokePrivate insertNotFull(i + 2)
+
+        assert(rt.toString == "(%s,%s,%s,%s)".format(i, i + 2, i + 4, i + 8))
+
+      }
+    }
+  }
+
+  it should "be able to insert into a non-full node" in {
+
+    val insertNotFull = PrivateMethod[Unit]('insertNotFull)
+
+    forAll { (i: Int) =>
+      whenever(i < Int.MaxValue - 24) {
+
+        var elems: Seq[Int] = Seq(1, 1, 2, 3, 9, 17, 20, 21, 23).map(_ + i)
+
+        var rt: BTree[Int] = new BTree(
+          Seq(elems(4)),
+          Seq(
+            new BTree(elems.slice(0, 4), Nil),
+            new BTree(elems.slice(5, 9), Nil)
+          )
+        )
+        assert(
+          rt.toString == "((%s,%s,%s,%s),%s,(%s,%s,%s,%s))".format(elems: _*) && elems == rt.elementsInOrder
+        )
+
+        rt invokePrivate insertNotFull(i + 12)
+
+        elems = (elems :+ (i + 12)).sorted
+
+        assert(
+          rt.toString == "((%s,%s,%s,%s),%s,(%s,%s,%s),%s,(%s))".format(elems: _*) && elems == rt.elementsInOrder
+        )
+
+      }
+    }
+  }
+
+  it should "be able to insert generally" in {
+
+    forAll { (ints: Seq[Int]) =>
+      whenever(ints.nonEmpty && ints.length < 1e5) {
+        var rt: BTree[Int] = BTree.empty(m = 5)
+        ints.foreach(rt.insert)
+      }
+    }
+
+  }
+
+}
